a8c619994f05c25a86a15a9f0b57e5339033d51d
ZaYtI <antonin_riquart@ens.univ-artois.fr>
2025-01-06 10:01:18 +0100

    🎉 First commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module poo.booksync.booksync {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens poo.booksync.booksync to javafx.fxml;
+    exports poo.booksync.booksync;
+}
