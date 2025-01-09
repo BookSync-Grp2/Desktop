0685060a1afe4833e106f1cd6c5b5ff207b4d6f6
ZaYtI <antonin_riquart@ens.univ-artois.fr>
2025-01-09 17:21:31 +0100

    🚧 Work in progress for javaFx navigation

--- src/main/java/poo/booksync/controller/LoginController.java
+++ src/main/java/poo/booksync/controller/LoginController.java
@@ -1,19 +1,31 @@
 package poo.booksync.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import poo.booksync.MainApplication;
 import poo.booksync.validators.EmailValidator;
 import poo.booksync.validators.FieldValidator;
 import poo.booksync.validators.PasswordValidator;
 
+import java.io.IOException;
+
 public class LoginController {
+    public Hyperlink registerhyperLink;
     @FXML
     private TextField emailField;
 
     @FXML
     private PasswordField passwordField;
+
+    @FXML
+    private Hyperlink registerHyperLink;
 
     @FXML
     private void initialize() {}
@@ -24,4 +36,10 @@ public class LoginController {
         String password = this.passwordField.getText();
         //TODO: Implémenter la logique de connexion avec l'API
     }
+
+    @FXML
+    private void redirectToRegister() {
+        MainApplication.redirectTo("register");
+    }
+
 }
