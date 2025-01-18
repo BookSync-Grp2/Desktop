cf05750036db95823feb427d81d7a67e7fca47f3
CamilleDlt <camille.delot11@gmail.com>
2025-01-18 15:46:03 +0100

    feat/Model : Ajout méthodes User & Librarian + Attribut  static currentUser

--- src/main/java/poo/booksync/model/Librarian.java
+++ src/main/java/poo/booksync/model/Librarian.java
@@ -11,4 +11,74 @@ public class Librarian extends User {
     public Librarian(int userId, String firstName, String lastName, String email, Date dateJoined, boolean validated, RoleType role) {
         super(userId, firstName, lastName, email, dateJoined, validated, role);
     }
+
+
+    public static User getUserById(int userId) {
+        for (User user : User.getUserList()) {
+            if (user.getUserId() == userId) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public boolean deleteUser(int userId) {
+        User userToRemove = getUserById(userId);
+        if (userToRemove != null) {
+            User.getUserList().remove(userToRemove);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean initializeUserList() {
+        // On vide la liste avant de la recharger
+        User.getUserList().clear();
+
+        // Ajouts d’utilisateurs (test)
+        //Remplacer par co avec la BDD qd ce sera possible
+        User.getUserList().add(new Librarian(
+                1, "Alice", "Martin", "alice.martin@biblio.com",
+                new Date(), true, RoleType.LIBRARIAN
+        ));
+        User.getUserList().add(new Librarian(
+                2, "Bob", "Dupond", "bob.dupond@biblio.com",
+                new Date(), true, RoleType.LIBRARIAN
+        ));
+        User.getUserList().add(new Client(
+                3, "Claire", "Durand", "claire.durand@example.com",
+                new Date(), false, RoleType.CLIENT
+        ));
+        User.getUserList().add(new Client(
+                4, "David", "Robert", "david.robert@example.com",
+                new Date(), false, RoleType.CLIENT
+        ));
+
+        return true;
+    }
+
+
+
+    public ArrayList<User> getAllUsers() {
+        return User.getUserList();
+    }
+
+    //Créer un USER manuellement
+    public boolean createUser(String firstName, String lastName, String email, RoleType role) {
+        //On prend la position de la liste pr nouvelle ID (A garder ?)
+        int newUserId = User.getUserList().size() + 1;
+
+        User newUser;
+        if (role == RoleType.CLIENT) {
+            newUser = new Client(newUserId, firstName, lastName, email, role);
+        } else {
+            newUser = new Librarian(newUserId, firstName, lastName, email, role);
+        }
+
+        //Envoie de nouvel utilisateur à la BDD
+
+        //Refresh de la liste des USER
+        this.initializeUserList();
+        return true;
+    }
 }
--- src/main/java/poo/booksync/model/User.java
+++ src/main/java/poo/booksync/model/User.java
@@ -1,9 +1,12 @@
 package poo.booksync.model;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Date;
 
 public abstract class User {
+
     private static ArrayList<User> userList = new ArrayList<User>();
+    private static User currentUser = null;
     private int userId;
     private String firstName;
     private String lastName;
@@ -32,5 +35,90 @@ public abstract class User {
         this.dateJoined = dateJoined;
         this.validated = validated;
         this.role = role;
+    }
+
+    public User(){}
+
+    //Setter pr l'User courant
+    public static void setCurrentUser(User user) {
+        currentUser = user;
+    }
+
+    //Methode static pr login
+    public static boolean login(String email, String password) {
+        //Verif en BDD si les paramètres sont bons.
+        //Si c'est ok on met le currentUser à jour.
+        //setCurrentUser(u);
+                //return true;
+        return false;
+    }
+
+    //Retourner les informations sur l'User courant
+    public User getCurrentUser(){
+        return currentUser;
+    }
+
+    //GETTER & SETTER
+    public static ArrayList<User> getUserList() {
+        return userList;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDateJoined() {
+        return dateJoined;
+    }
+    public void setDateJoined(Date dateJoined) {
+        this.dateJoined = dateJoined;
+    }
+
+    public boolean isValidated() {
+        return validated;
+    }
+    public void setValidated(boolean validated) {
+        this.validated = validated;
+    }
+
+    public RoleType getRole() {
+        return role;
+    }
+    public void setRole(RoleType role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "ID=" + userId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", dateJoined=" + dateJoined +
+                ", validated=" + validated +
+                ", role=" + role +
+                '}';
     }
 }
